c782545b8f128602ae90adc465f74f83f22d8024
spragha <sraghavan4031@gmail.com>
2016-05-06 19:41:09 +0530

    adding Spring-crud intial template

---
+++ src/main/java/com/mrk/bankacc/Account.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mrk.bankacc;
+
+import java.security.acl.Owner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.joda.time.DateTime;
+import org.springframework.beans.support.MutableSortDefinition;
+import org.springframework.beans.support.PropertyComparator;
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ * Simple business object representing a pet.
+ *
+ * @author Ken Krebs
+ * @author Juergen Hoeller
+ * @author Sam Brannen
+ */
+@Entity
+@Table(name = "account")
+public class Account{
+
+    @Column(name = "acc_creation_date")
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private DateTime accCreationDate;
+    
+    @Column(name = "acc_number")
+    @NotEmpty
+    private String accNumber;
+    
+    @Column(name = "acc_type")
+    @NotEmpty
+    private Integer accType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Customer customer;
+
+
+    public void setAccCreationDate(DateTime accCreationDate) {
+        this.accCreationDate = accCreationDate;
+    }
+
+    public DateTime getAccCreationDate() {
+        return this.accCreationDate;
+    }
+
+    protected void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Customer getCustomer() {
+        return this.customer;
+    }
+
+	public String getAccNumber() {
+		return accNumber;
+	}
+
+	public void setAccNumber(String accNumber) {
+		this.accNumber = accNumber;
+	}
+
+	public Integer getAccType() {
+		return accType;
+	}
+
+	public void setAccType(Integer accType) {
+		this.accType = accType;
+	}
+
+}
